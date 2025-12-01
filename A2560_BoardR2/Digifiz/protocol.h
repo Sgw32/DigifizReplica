a039c23a2bac577c9147c8a01f7b0e3f9fc47f40
Fedor Zagumennov <sgw32nc@gmail.com>
2025-12-02 02:27:52 +0300

    Add quadratic RPM calibration support

--- A2560_BoardR2/Digifiz/protocol.h
+++ A2560_BoardR2/Digifiz/protocol.h
@@ -57,6 +57,7 @@ typedef enum {
     PARAMETER_SET_SIGNAL_OPTIONS,         // 42
     PARAMETER_MAX_RPM_THRESHOLD,        // 43
     PARAMETER_MAX_SPEED_THRESHOLD,      // 44
+    PARAMETER_RPM_QUADRATIC_COEFFICIENT = 53,
     // Additional parameters and functions
     PARAMETER_UPTIME = 123,
     PARAMETER_READ_ADDITION = 128,
