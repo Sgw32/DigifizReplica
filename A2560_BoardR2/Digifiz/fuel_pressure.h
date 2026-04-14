dbb6d6cb154f384048082bd773b6a85bf5661391
Fedor Zagumennov <sgw32nc@gmail.com>
2026-04-14 17:01:01 +0300

    docs: expand doxygen coverage for A2560 headers

--- A2560_BoardR2/Digifiz/fuel_pressure.h
+++ A2560_BoardR2/Digifiz/fuel_pressure.h
@@ -1,12 +1,35 @@
 #ifndef FUEL_PRESSURE_H
 #define FUEL_PRESSURE_H
+/**
+ * @file fuel_pressure.h
+ * @brief Fuel pressure sensing interface for the A2560 board firmware.
+ */
+
 #include "Arduino.h"
 #include "setup.h"
 
+/**
+ * @brief Initializes ADC/pin resources used by the fuel pressure sensor.
+ */
 void initFuelPressureSensor();
 
+/**
+ * @brief Samples and filters the fuel pressure signal.
+ */
 void processFuelPressure();
+
+/**
+ * @brief Returns the latest computed fuel pressure in bar.
+ *
+ * @return Fuel pressure value.
+ */
 float getFuelPressure();
+
+/**
+ * @brief Returns the latest measured sensor voltage.
+ *
+ * @return Fuel pressure sensor voltage.
+ */
 float getFuelPressureVoltage();
 
 #endif 
